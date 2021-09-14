a6a08dda4b1bb3e7b23b4d84944ea152ad6f94f4
Adam Jonsson <adamsmajl@gmail.com>
2021-09-14 08:34:34 +0200

    #8 feat: Desktop controls (#14)
    * feat: Add basic desktop controlls for development
    
    * doc: Add simpel documentation how to get started

--- Assets/Shared/Scripts/LevelManager.cs
+++ Assets/Shared/Scripts/LevelManager.cs
@@ -5,11 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    [Header("If active, press enter or two fingers to skip to next level")]
+    [SerializeField] private bool _enableLevelSkipMode = false;
+
+    [Header("Level settings")]
     [SerializeField] private int _startLevelIndex = 0;
     [SerializeField] private List<string> _levels;
+
     private int _currentLevelIndex = 0;
 
     private Pose _levelSpawnPosition = new Pose();
+
+    private ARSessionSetup _arSessionSetup;
 
     public Pose LevelSpawnPosition {
         get => _levelSpawnPosition;
@@ -19,12 +26,22 @@ public class LevelManager : MonoBehaviour
     {
         DontDestroyOnLoad(this.gameObject);
         _currentLevelIndex = _startLevelIndex;
+
     }
 
     void Update()
     {
-        //OBS! This is temporary, delete this when we have a game logic
-        if (Input.touchCount == 2 && Input.GetTouch(1).phase == TouchPhase.Began)
+        if (this._enableLevelSkipMode)
+        {
+            CheckForSkipInput();
+        }
+    }
+
+    public void CheckForSkipInput()
+    {
+        bool mobileSkip = Input.touchCount == 2 && Input.GetTouch(1).phase == TouchPhase.Began;
+        bool desktopSkip = Input.GetKeyDown(KeyCode.Return);
+        if (mobileSkip | desktopSkip)
         {
             this.LoadNextLevel();
         }
