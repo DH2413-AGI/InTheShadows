1787f3567b1f1cac4ec2e43ec39b0d3a5fdb83dc
Adam Jonsson <adamsmajl@gmail.com>
2021-09-20 09:08:55 +0200

    fix: Change name of new levels

--- Assets/Shared/Scripts/ScaleObject.cs
+++ Assets/Shared/Scripts/ScaleObject.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class ScaleObject : MonoBehaviour
 {
-    private Vector3 scaleChange;
-    // Start is called before the first frame update
-    void Start()
-    {
-        scaleChange = new Vector3(-0.0f, -0.01f, -0.0f);
-    }
+    [SerializeField] private Vector3 scaleChange = new Vector3(0.0f, -0.2f, 0.0f);
+
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.localScale += scaleChange;
+        gameObject.transform.localScale += scaleChange * Time.deltaTime;
         
         // Move the object up and down
         if (gameObject.transform.localScale.y < 0.2f || gameObject.transform.localScale.y > 1.0f)
